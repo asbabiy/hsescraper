d23b5ce337dc5ddc6b21d24ab22dfd9322ad60c3
Alexander Babii <55639869+asbabiy@users.noreply.github.com>
2021-01-09 08:25:16 +0300

    Update db.py

--- app/db.py
+++ app/db.py
@@ -5,9 +5,10 @@ import streamlit as st
 @st.cache
 def get_db(filename):
     url = 'https://media.githubusercontent.com/media/asbabiy/hsescraper/main/app/posts.db'
-    r = requests.get(url)
 
-    with open(f'app/{filename}.db', 'wb') as db:
-        db.write(r.content)
+    if not os.path.isfile(f"{filename}.db"):
+        with open(f'app/{filename}.db', 'wb') as db:
+            r = requests.get(url)
+            db.write(r.content)
 
-    return len(r.content)
+    # return len(r.content)
